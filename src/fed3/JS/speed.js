037c3ecc0e94bab7fff79ed49b50b6881b6556bd
greta marcattilj <gretamarcattilj@gmail.com>
2019-10-23 17:20:25 +0200

    modifiche cartelle

---
+++ src/fed3/JS/speed.js
@@ -0,0 +1,19 @@
+function speed(dist, time) {
+    if (typeof (dist) !== "number" || typeof (time) !== "number" || dist <= 0 || time <= 0) {
+        return undefined
+    } else {
+        return dist / time;
+    }
+}
+
+console.log(speed("2", 3) === undefined);
+console.log(speed(2, "3") === undefined);
+console.log(speed("ciao", "peppo") === undefined);
+console.log(speed(0, 3) === undefined);
+console.log(speed(2, -3) === undefined);
+console.log(speed(-2, 3) === undefined);
+console.log(speed(2, 0) === undefined);
+console.log(Boolean(speed(2, 0.5)) === true);
+console.log(Boolean(speed(2.4, 3)) === true);
+console.log(Boolean(speed(2, Infinity)) === false);
+console.log(Boolean(speed(Infinity, 2)) === true);
