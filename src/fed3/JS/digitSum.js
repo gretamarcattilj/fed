68135c4768efbe9e935a3a1ef9e684a900fc2855
greta marcattilj <gretamarcattilj@gmail.com>
2019-10-24 09:35:52 +0200

    modifca funzione digitSum

--- src/fed3/JS/digitSum.js
+++ src/fed3/JS/digitSum.js
@@ -1,9 +1,21 @@
 function digitSum(num) {
-    let n;
-    let sum;
-    while (num % 10 === 0) {
+    let sum = 0;
+    while (num % 10 !== 0) {
         sum += num % 10;
         num = Math.floor(num / 10);
     }
+    return sum;
 }
-console.log(digitSum(1256743));
+console.log(digitSum(1856743));
+
+function digitSumNigel(num) {
+    num = num.toString();
+    // ATTENZIONE al diverso modo di usare le funzioni note come toString() e Number()
+    // ATTENZIONE JS è case-sensitive
+    let sum = 0;
+    for (i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+    return sum;
+}
+console.log(digitSumNigel(1856743));
