0f80127f07c08e237c3ebf70dea134421f6e508d
greta marcattilj <gretamarcattilj@gmail.com>
2019-10-29 12:34:21 +0100

    merge

---
+++ src/fed3/JS/merge.js
@@ -0,0 +1,11 @@
+function merg(left, right) {
+    result = "";
+
+    for (let i = 0; i < left.length; i++) {
+        for (let j = 0; j < right.length; j++){
+            if(left[i]<)
+        }
+    }
+}
+
+console.log([1, 2, 3, 4], [2, 3, 5, 6, 9]);
