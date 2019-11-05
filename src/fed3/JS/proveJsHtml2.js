44179842201f5471e2c230a5d01031f2d74598cf
greta marcattilj <gretamarcattilj@gmail.com>
2019-11-05 12:30:22 +0100

    prove html js

--- src/fed3/JS/proveJsHtml2.js
+++ src/fed3/JS/proveJsHtml2.js
@@ -1,3 +1,9 @@
+window.onload = () => {
+    let target = document.getElementById("target");
+    target.textContent = "Hello!"
+    target.style.fontSize = "50px";
+}
+
 function check() {
     let name = document.getElementById("user").value.split(" ");
     console.log(name)
@@ -9,12 +15,67 @@ function check() {
     }
 }
 
-function changeBKcolor(){
-    let colore=document.getElementById("colore").value;
-    if(colore===""){
+function changeBKcolor() {
+    let colore = document.getElementById("colore").value;
+    if (colore === "") {
         window.alert("colore non corretto");
     }
-    else{
-        document.bgColor=colore;
+    else {
+        document.bgColor = colore;
     }
+}
+
+function changeColorSelect() {
+    let colore = document.getElementById("sceltacolore").value;
+    let imm = document.getElementById("immagine");
+    if (colore === "") {
+        window.alert("colore non corretto");
+    }
+    else {
+        document.bgColor = colore;
+    }
+    if (colore === "red") {
+        imm.src = "../fed1/img/gattoRosso.jpg"
+    }
+    else if (colore === "green") {
+        imm.src = "../fed1/img/gattoVerde.jpg"
+    }
+    else if (colore === "blue") {
+        imm.src = "../fed1/img/gattoBlu.jpg"
+    }
+    else if (colore === "gold") {
+        imm.src = "../fed1/img/gattoOro.jpg"
+    }
+}
+
+function impostazioniIn() {
+    console.log("IN");
+}
+
+function impostazioniOut() {
+    console.log("OUT");
+}
+
+function sopraImm() {
+    let imm = document.getElementById("immagine");
+    imm.style.width = (imm.clientWidth + 150) + "px";
+    document.fgColor = "green";
+    document.bgColor = "lightgreen";
+    console.log("GATTINO");
+}
+
+function fuoriImm() {
+    let imm = document.getElementById("immagine");
+    imm.style.width = (imm.clientWidth - 150) + "px";
+    document.fgColor = "purple";
+    document.bgColor = "gold";
+    console.log("NO MORE GATTINO");
+}
+
+function zoomText() {
+    document.getElementById('target').style.fontSize = "100px";
+}
+
+function deZoomText() {
+    document.getElementById('target').style.fontSize = "50px";
 }
