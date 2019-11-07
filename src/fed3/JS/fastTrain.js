5274c2227015fdc27275784ce67ce3b74c7c9b2d
greta marcattilj <gretamarcattilj@gmail.com>
2019-11-07 11:42:39 +0100

    modifiche

---
+++ src/fed3/JS/fastTrain.js
@@ -0,0 +1,3 @@
+function fastTrain(numbers) {
+   
+}
--- src/fed3/JS/presentation.js
+++ src/fed3/JS/presentation.js
@@ -20,4 +20,16 @@ function save() {
     let job = document.getElementById("job");
     nome.value = json.name;
     job.value = json.job;
+    let options = document.getElementsByClassName("check");
+    let knows = json.linguaggi;
+    for (let i = 0; i < options.length; i++) {
+        for (let j = 0; j < knows.length; j++) {
+            if (options[i].value === knows[j]) {
+                options[i].checked = true;
+                break;
+            } else {
+                options[i].checked = false;
+            }
+        }
+    }
 }
