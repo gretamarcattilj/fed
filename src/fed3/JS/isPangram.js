229a65054c78c7014fbd9a07b3f9ae8028991b8e
greta marcattilj <gretamarcattilj@gmail.com>
2019-11-11 14:02:09 +0100

    aggiunta possibilità debug

--- src/fed3/JS/isPangram.js
+++ src/fed3/JS/isPangram.js
@@ -23,9 +23,35 @@ function isPangram(str) {
     return true;
 }
 
-console.log(isPangram("The quick brown fox jumps over the lazy dog")===true);
-console.log(isPangram("i topi non avevano nipoti")===false);
-console.log(isPangram("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")===false);
-console.log(isPangram(56)===undefined);
-console.log(isPangram(true)===undefined);
-console.log(isPangram("true")===false);
+function isPangram(str) {
+    if (typeof (str) !== "string") {
+        return undefined;
+    }
+    if (str.length < 21) {
+        return false;
+    }
+    let aux = Set([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]);
+    let indAux = 0;
+    for (let i = 0; i < str.length; i++) {
+        indAux = aux.indexOf(str[i].toLowerCase());
+        if (indAux === -1) {
+            continue;
+        } else {
+            aux = aux.replace(str[i].toLowerCase(), "0");
+        }
+    }
+    for (let i = 0; i < aux.length; i++) {
+        if (aux[i] !== "0") {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog") === true);
+console.log(isPangram("i topi non avevano nipoti") === false);
+console.log(isPangram("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") === false);
+console.log(isPangram(56) === undefined);
+console.log(isPangram(true) === undefined);
+console.log(isPangram("true") === false);
