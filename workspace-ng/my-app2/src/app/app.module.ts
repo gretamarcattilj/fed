14e419a968f431b8375ab31f48aadf05c3286d6f
greta marcattilj <gretamarcattilj@gmail.com>
2019-11-18 13:57:15 +0100

    user, users

--- workspace-ng/my-app2/src/app/app.module.ts
+++ workspace-ng/my-app2/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
