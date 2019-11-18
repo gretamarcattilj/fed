3ba42366abf3b1758ca808f12c7d5b59406c5584
greta marcattilj <gretamarcattilj@gmail.com>
2019-11-18 17:45:04 +0100

    modifche

--- workspace-ng/my-oved/src/app/groups/groups.component.ts
+++ workspace-ng/my-oved/src/app/groups/groups.component.ts
@@ -7,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsComponent implements OnInit {
   groups: string[];
+  members: string[];
   constructor() {
     this.groups = ["red", "blue", "green", "yellow"];
   }
 
   ngOnInit() {
+  }
+  onChange(id: HTMLSelectElement) {
+    let oved: string = id.value;
+    if (oved == "red") {
+      this.members = ["alberto", "greta", "maurizio"];
+    } else if (oved == "blue") {
+      this.members = ["aurora", "luca", "marianna"];
+    } else if (oved == "green") {
+      this.members = ["yun", "marco", "nadia", "nigel"];
+    } else {
+      this.members = ["jacopo", "marco", "roberta"];
+    }
   }
 
 }
--- workspace-ng/my-oved/src/app/members/members.component.ts
+++ workspace-ng/my-oved/src/app/members/members.component.ts
@@ -6,23 +6,10 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./members.component.css']
 })
 export class MembersComponent implements OnInit {
-  members: string[];
+ 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onChange(id: HTMLSelectElement) {
-    let oved: string = id.value;
-    if (oved == "red") {
-      this.members = ["alberto", "greta", "maurizio"];
-    } else if (oved == "blue") {
-      this.members = ["aurora", "luca", "marianna"];
-    } else if (oved == "green") {
-      this.members = ["yun", "marco", "nadia", "nigel"];
-    } else {
-      this.members = ["jacopo", "marco", "roberta"];
-    }
   }
 
 }
